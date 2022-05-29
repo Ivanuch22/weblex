44d153f8181f999a9c75f72442cd2a3485b16086
Ivan <ivan.kalunuch12@gmail.com>
2022-05-29 11:48:19 +0300

    test first section mobile version

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,23 @@
-const log = document.querySelector('');
+const menu = document.querySelector('.menu');
+const body = document.querySelector('body');
+
+const changeCssStyleMenuWhenScrolling = () => {
+    window.addEventListener('scroll', () => {
+        if (window.screen.width <= 650) {
+            if (window.pageYOffset >= 50) {
+                menu.style.cssText = `
+                z-index:22;
+                top: 0;
+                position: fixed;
+                `;
+            } else if (window.pageYOffset < 50) {
+                menu.style.cssText = `
+                z-index:22;
+                top: 50px;
+                position: absolute;
+                `;
+            }
+        }
+    })
+}
+changeCssStyleMenuWhenScrolling()
